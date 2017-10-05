e35a8c17e7e9baebc2c6ea777fe8b8da149e2fe1
Timothy Le <timothyle85@gmail.com>
2017-10-05 11:08:57 -0700

    Style and functional

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+// Fade in function
+var el = document.getElementById("fadein");
+
+function fadeIn(el) {
+  el.style.opacity = 0;
+
+
+  var tick = function() {
+    el.style.opacity = +el.style.opacity + 0.01;
+
+
+    if (+el.style.opacity < 1) {
+      (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16)
+    }
+  };
+
+  tick();
+}
+
+fadeIn(el);
