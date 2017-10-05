91b2179d2dd20ccfc5c449961dd63840414acbad
Timothy Le <timothyle85@gmail.com>
2017-10-05 11:12:11 -0700

    Additional styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,24 @@ function fadeIn(el) {
 }
 
 fadeIn(el);
+
+// Longer fade in
+var el = document.getElementById("fadein2");
+
+function fadeIn(el) {
+  el.style.opacity = 0;
+
+
+  var tick = function() {
+    el.style.opacity = +el.style.opacity + 0.01;
+
+
+    if (+el.style.opacity < 1) {
+      (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 32)
+    }
+  };
+
+  tick();
+}
+
+fadeIn(el);
